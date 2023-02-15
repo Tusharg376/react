41b5e8f3be1ecf3d0b9690013fd47ab98c7e295c
Tusharg376 <honeygautam0@gmail.com>
2023-02-15 19:40:22 +0530

    assignment done

--- google-page/src/App.js
+++ google-page/src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import Logo from './components/Logo.js';
-import Input from './components/input.js';
-import SearchButton from './components/SearchButton';
-import LuckyButton from './components/LuckyButton';
+import React,{useState} from "react";
 
 export default function App() {
+  const [para,setPara] = useState("")
+  const handleChange = (event) => {
+    //console.log(event.target.value)
+    setPara(event.target.value)
+  }
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        flexDirection: 'column',
-        alignItems: 'center',
-      }}
-    >
-      <Logo />
-      <Input />
-      <br />
-      <div style={{ margin: '5px' }}>
-        <SearchButton />
-        <LuckyButton />
-      </div>
+    <div>
+     your paragraph here <input value={para} onChange={handleChange}/>
+     <button onClick = {() => {
+     let newPara = para
+       setPara(newPara.toUpperCase())
+     }}> Submit </button>
+     <p>{para}</p>
     </div>
   );
 }
--- google-page/src/components/LuckyButton.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import Button from "./button"
-function LuckyButton(){
-  return(
-    < Button text = "I am feeling lucky" />
-  )
-}
-export default LuckyButton
